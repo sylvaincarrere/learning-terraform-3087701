4538010b40cd148be9ac55511b2fec28a1130f4e
sylvaincarrere <carrere.sylvain@gmail.com>
2024-08-29 11:11:41 +0200

    fix security_group_id

--- main.tf
+++ main.tf
@@ -37,37 +37,31 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  name = "blog"
   type = "ingress"
-
   from_port = 80
   to_port = 80
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  name = "blog"
   type = "ingress"
-
   from_port = 443
   to_port = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  name = "blog"
   type = "egress"
-
   from_port = 0
   to_port = 0
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
