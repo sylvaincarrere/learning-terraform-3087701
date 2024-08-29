8d805970a2de7311f796d40c5dc38a302e37b8f8
sylvaincarrere <carrere.sylvain@gmail.com>
2024-08-29 11:06:50 +0200

    aws security group

--- main.tf
+++ main.tf
@@ -14,11 +14,60 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http & https in. Everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  name = "blog"
+  type = "ingress"
+
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  aws_security_group = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  name = "blog"
+  type = "ingress"
+
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  aws_security_group = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  name = "blog"
+  type = "egress"
+
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  aws_security_group = aws_security_group.blog.id
 }
